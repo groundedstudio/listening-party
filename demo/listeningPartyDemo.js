d3fb3ed9741a850192819c9e258dc7c2feeba8e9
Sam Drew <sam-drew@outlook.com>
2020-05-05 23:35:16 +0100

    :bug: Wait for DOM to properly load.

--- demo/listeningPartyDemo.js
+++ demo/listeningPartyDemo.js
@@ -18,128 +18,129 @@ function formatHoursOrMins (num) {
         return(num);
     }
 }
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelector('#playAtTime').innerHTML = formatHoursOrMins(countDownDate.getHours()) + ":" + formatHoursOrMins(countDownDate.getMinutes());
 
-document.querySelector('#playAtTime').innerHTML = formatHoursOrMins(countDownDate.getHours()) + ":" + formatHoursOrMins(countDownDate.getMinutes());
+    // Update the count down every 1 second
+    var x = setInterval(function() {
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+        // Get today's date and time
+        var now = new Date().getTime();
 
-    // Get today's date and time
-    var now = new Date().getTime();
+        // Find the distance between now and the count down date
+        var distance = countDownDate.getTime() - now;
 
-    // Find the distance between now and the count down date
-    var distance = countDownDate.getTime() - now;
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        document.querySelector("#countdown").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
 
-    document.querySelector("#countdown").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.querySelector("#countdown").innerHTML = "The listening party has started!";
+        }
+    }, 1000);
 
-    // If the count down is finished, write some text
-    if (distance < 0) {
-        clearInterval(x);
-        document.querySelector("#countdown").innerHTML = "The listening party has started!";
-    }
-}, 1000);
+    // Determine if the Spotify Web SDK should be loaded.
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    // Check that there is an access token provided.
+    if (urlParams.get('code') != null) {
+        // Add the Spotify SDK script tag to the DOM.
+        var spotifySDK = document.createElement('script');
+        spotifySDK.setAttribute('src','https://sdk.scdn.co/spotify-player.js');
+        document.head.appendChild(spotifySDK);
 
-// Determine if the Spotify Web SDK should be loaded.
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-// Check that there is an access token provided.
-if (urlParams.get('code') != null) {
-    // Add the Spotify SDK script tag to the DOM.
-    var spotifySDK = document.createElement('script');
-    spotifySDK.setAttribute('src','https://sdk.scdn.co/spotify-player.js');
-    document.head.appendChild(spotifySDK);
+        // Swap the 'connect spotify' and 'play @' buttons for a
+        // 'spotify will play in...' text.
+        document.getElementById('playAt').style.display = "none";
+        document.getElementById('connectWithSpotify').style.display = "none";
+        document.getElementById('spotifyWillPlayIn').style.display = "inline";
 
-    // Swap the 'connect spotify' and 'play @' buttons for a
-    // 'spotify will play in...' text.
-    document.getElementById('playAt').style.display = "none";
-    document.getElementById('connectWithSpotify').style.display = "none";
-    document.getElementById('spotifyWillPlayIn').style.display = "inline";
-
-    // When playback SDK is ready, generate player with user's code.
-    window.onSpotifyWebPlaybackSDKReady = () => {
-        const token = urlParams.get('code');
-        const player = new Spotify.Player({
-            name: 'Listening Party',
-            getOAuthToken: cb => { cb(token); }
-        });
-
-        // Error handling
-        player.addListener('initialization_error', ({ message }) => { console.error(message); });
-        player.addListener('authentication_error', ({ message }) => { console.error(message); });
-        player.addListener('account_error', ({ message }) => { console.error(message); });
-        player.addListener('playback_error', ({ message }) => { console.error(message); });
-
-        // Playback status updates
-        player.addListener('player_state_changed', state => { console.log(state); });
-
-        // Ready
-        player.addListener('ready', ({ device_id }) => {
-            console.log('Ready with Device ID', device_id);
-        });
-
-        // Not Ready
-        player.addListener('not_ready', ({ device_id }) => {
-            console.log('Device ID has gone offline', device_id);
-        });
-
-        // Connect to the player!
-        player.connect();
-
-        const play = ({
-            spotify_uri,
-            playerInstance: {
-                _options: {
-                    getOAuthToken,
-                    id
-                }
-            }
-        }) => {
-            getOAuthToken(access_token => {
-                fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
-                    method: 'PUT',
-                    body: JSON.stringify({ context_uri: spotify_uri }),
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${access_token}`
-                    },
-                });
+        // When playback SDK is ready, generate player with user's code.
+        window.onSpotifyWebPlaybackSDKReady = () => {
+            const token = urlParams.get('code');
+            const player = new Spotify.Player({
+                name: 'Listening Party',
+                getOAuthToken: cb => { cb(token); }
             });
-        };
 
-        // Update the count down every 1 second
-        var y = setInterval(function() {
+            // Error handling
+            player.addListener('initialization_error', ({ message }) => { console.error(message); });
+            player.addListener('authentication_error', ({ message }) => { console.error(message); });
+            player.addListener('account_error', ({ message }) => { console.error(message); });
+            player.addListener('playback_error', ({ message }) => { console.error(message); });
 
-            // Get today's date and time
-            var now = new Date().getTime();
+            // Playback status updates
+            player.addListener('player_state_changed', state => { console.log(state); });
 
-            // Find the distance between now and the count down date
-            var distance = countDownDate.getTime() - now;
+            // Ready
+            player.addListener('ready', ({ device_id }) => {
+                console.log('Ready with Device ID', device_id);
+            });
 
-            // If the count down is finished, and the user is within 2 seconds
-            // of the party start time, clear the countdown and play the resource.
-            if ((distance < 0) && (distance > -2)) {
-                clearInterval(y);
-                play({
-                    playerInstance: player,
-                    spotify_uri: 'spotify:album:7dK54iZuOxXFarGhXwEXfF',
+            // Not Ready
+            player.addListener('not_ready', ({ device_id }) => {
+                console.log('Device ID has gone offline', device_id);
+            });
+
+            // Connect to the player!
+            player.connect();
+
+            const play = ({
+                spotify_uri,
+                playerInstance: {
+                    _options: {
+                        getOAuthToken,
+                        id
+                    }
+                }
+            }) => {
+                getOAuthToken(access_token => {
+                    fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
+                        method: 'PUT',
+                        body: JSON.stringify({ context_uri: spotify_uri }),
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': `Bearer ${access_token}`
+                        },
+                    });
                 });
-            // If the count down has finished and the user is after the time that
-            // the part is due to finish, tell them.
-            } else if (distance < (Object.keys(tracklist)[Object.keys(tracklist).length - 1])) {
-                clearInterval(y);
-                document.getElementById('spotifyWillPlayIn').innerHTML = "The party has ended!";
-            // If the count down has finished and the user has joined during the
-            // party session, seek/skip them to the right place.
-            } else if ((distance < 0)(distance > (Object.keys(tracklist)[Object.keys(tracklist).length - 1]))) {
-                console.log("Skip to correct position");
             };
-        }, 1000);
+
+            // Update the count down every 1 second
+            var y = setInterval(function() {
+
+                // Get today's date and time
+                var now = new Date().getTime();
+
+                // Find the distance between now and the count down date
+                var distance = countDownDate.getTime() - now;
+
+                // If the count down is finished, and the user is within 2 seconds
+                // of the party start time, clear the countdown and play the resource.
+                if ((distance < 0) && (distance > -2)) {
+                    clearInterval(y);
+                    play({
+                        playerInstance: player,
+                        spotify_uri: 'spotify:album:7dK54iZuOxXFarGhXwEXfF',
+                    });
+                // If the count down has finished and the user is after the time that
+                // the part is due to finish, tell them.
+                } else if (distance < (Object.keys(tracklist)[Object.keys(tracklist).length - 1])) {
+                    clearInterval(y);
+                    document.getElementById('spotifyWillPlayIn').innerHTML = "The party has ended!";
+                // If the count down has finished and the user has joined during the
+                // party session, seek/skip them to the right place.
+                } else if ((distance < 0)(distance > (Object.keys(tracklist)[Object.keys(tracklist).length - 1]))) {
+                    console.log("Skip to correct position");
+                };
+            }, 1000);
+        };
     };
-};
+})
