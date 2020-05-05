dd821c46315a2ef882c1980db7e941f7718d5b78
Sam Drew <sam-drew@outlook.com>
2020-05-05 23:28:44 +0100

    :recycle: Move countdown text up the script.

--- demo/listeningPartyDemo.js
+++ demo/listeningPartyDemo.js
@@ -10,6 +10,42 @@ const tracklist = {
     687: "end",
 };
 
+// Countdown text stuff.
+function formatHoursOrMins (num) {
+    if (num < 10) {
+        return("0" + num);
+    } else {
+        return(num);
+    }
+}
+
+document.querySelector('#playAtTime').innerHTML = formatHoursOrMins(countDownDate.getHours()) + ":" + formatHoursOrMins(countDownDate.getMinutes());
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate.getTime() - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.querySelector("#countdown").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.querySelector("#countdown").innerHTML = "The listening party has started!";
+    }
+}, 1000);
+
 // Determine if the Spotify Web SDK should be loaded.
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
@@ -107,39 +143,3 @@ if (urlParams.get('code') != null) {
         }, 1000);
     };
 };
-
-// Countdown text stuff.
-function formatHoursOrMins (num) {
-    if (num < 10) {
-        return("0" + num);
-    } else {
-        return(num);
-    }
-}
-
-document.querySelector('#playAtTime').innerHTML = formatHoursOrMins(countDownDate.getHours()) + ":" + formatHoursOrMins(countDownDate.getMinutes());
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-    // Get today's date and time
-    var now = new Date().getTime();
-
-    // Find the distance between now and the count down date
-    var distance = countDownDate.getTime() - now;
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    document.querySelector("#countdown").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-
-    // If the count down is finished, write some text
-    if (distance < 0) {
-        clearInterval(x);
-        document.querySelector("#countdown").innerHTML = "The listening party has started!";
-    }
-}, 1000);
